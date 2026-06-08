9abc73830c7405f2b28bf2856abda9b0518fd5ec
Devadharshini877 <devadharshini.m87@gmail.com>
2026-06-08 11:45:12 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# MERN Stack Internship
+
+## Day 1 - Variables and Data Types
+
+Topics Covered:
+- Variables
+- String
+- Number
+- Boolean
+- Array
+
+Output:
+
+Name: Devadharshini
+Age: 19
+Student: true
+City: Namakkal
+Skills: [ 'HTML', 'CSS', 'JavaScript' ]
+
+Created by:
+Devadharshini
+M. Kumarasamy College of Engineering
---
+++ day1.js
@@ -0,0 +1,15 @@
+// Day 1 - Variables and Data Types
+
+let name = "Devadharshini";
+let age = 19;
+let isStudent = true;
+let city = "Namakkal";
+
+let skills = ["HTML", "CSS", "JavaScript"];
+
+// Display Output
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
+console.log("City:", city);
+console.log("Skills:", skills);
